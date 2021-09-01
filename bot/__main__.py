06a718ad833accfeb5173f95c4dceb9176d947bc
NOBITA <88440061+Nobita-o@users.noreply.github.com>
2021-09-02 02:40:57 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,6 +12,7 @@ from telegram.ext import CommandHandler
 from wserver import start_server_async
 from bot import bot, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, IS_VPS, SERVER_PORT
 from bot.helper.ext_utils import fs_utils
+from bot.modules import logox.png
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
@@ -44,23 +45,23 @@ def stats(update, context):
 
 
 def start(update, context):
-    buttons = button_build.ButtonMaker()
-    buttons.buildbutton("⚡ Repo", "https://github.com/nobita-o/nobita-mirrorbot")
-    buttons.buildbutton("⭐ OWNER", "https://t.me/nobita_o")
-    reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
-    if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
-        start_string = f'''
+    start_string = f'''
 This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
-        sendMarkup(start_string, context.bot, update, reply_markup)
-    else:
-        sendMarkup(
-            'Oops! not a Authorized user.\nPlease deploy your own <b>slam-mirrorbot</b>.',
-            context.bot,
-            update,
-            reply_markup,
-        )
+    buttons = button_build.ButtonMaker()
+    buttons.buildbutton("Repo", "https://github.com/nobita-o/Nobita-MirrorBot")
+    buttons.buildbutton("Owner", "https://t.me/nobita_o")
+    reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
+    LOGGER.info('UID: {} - UN: {} - MSG: {}'.format(update.message.chat.id, update.message.chat.username, update.message.text))
+    uptime = get_readable_time((time.time() - botStartTime))
+    if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
+        if update.message.chat.type == "private" :
+            sendMessage(f"Hey I'm Alive 🙂\nSince: <code>{uptime}</code>", context.bot, update)
+        else :
+            sendMarkup(logox.png, start_string, context.bot, update, reply_markup)
+    else :
+        sendMarkup(f"Oops! You are not allowed to use me.</b>.", context.bot, update, reply_markup)
 
 
 def restart(update, context):
